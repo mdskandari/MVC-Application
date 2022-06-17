778f4655d0ae8651a023f8aa6f00ad45348f1472
Mohammad Eskandari <mdskandari@outlook.com>
2022-06-17 20:56:22 +0430

    database info loaded from .env file

--- config/database.php
+++ config/database.php
@@ -7,14 +7,14 @@ use \Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule();
 
 $capsule->addConnection([
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'port' => '3306',
-    'database' => 'mvc_application',
-    'username' => 'root',
-    'password' => '123456789',
-    'charset' => 'utf8',
-    'collation' => 'utf8_general_ci'
+    'driver' => $_ENV['DB_CONNECTION'],
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'database' => $_ENV['DB_DATABASE'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'charset' => $_ENV['DB_CHARSET'],
+    'collation' => $_ENV['DB_COLLATION']
 ]);
 
 
