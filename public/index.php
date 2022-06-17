459d1cca48fe20a3d6649e874773432f8a839420
Mohammad Eskandari <mdskandari@outlook.com>
2022-06-17 20:55:35 +0430

    dotENV loaded

--- public/index.php
+++ public/index.php
@@ -7,6 +7,9 @@ error_reporting(E_ALL);
 set_error_handler('Core\Error::errorHandler');
 set_exception_handler('Core\Error::exceptionHandler');
 
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeLoad();
+
 
 $router = require "../config/Web.php";
 $router->dispatch($_SERVER['QUERY_STRING']);
