ad9eeddc0eee68bf3e35852a156ffd5de44afb1c
Mohammad Eskandari <mdskandari@outlook.com>
2022-06-17 16:15:15 +0430

    error handling

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Requested URL: " . $_SERVER['QUERY_STRING'];
+require '../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('Core\Error::exceptionHandler');
+
+
+$router = require "../App/Web.php";
+$router->dispatch($_SERVER['QUERY_STRING']);
+
+
+
+
