106275e45adf63d67738e288292779741e08f825
Mohammad Eskandari <mdskandari@outlook.com>
2022-06-17 18:47:22 +0430

    database config file loaded

--- public/index.php
+++ public/index.php
@@ -1,14 +1,14 @@
 <?php
 
 require '../vendor/autoload.php';
+require '../config/database.php';
 
 error_reporting(E_ALL);
-//extract(['debug' => true], EXTR_SKIP);
 set_error_handler('Core\Error::errorHandler');
 set_exception_handler('Core\Error::exceptionHandler');
 
 
-$router = require "../App/Web.php";
+$router = require "../config/Web.php";
 $router->dispatch($_SERVER['QUERY_STRING']);
 
 
