2800169408ca427b7d290f66f599a4602a807472
Mohammad Eskandari <mdskandari@outlook.com>
2022-06-17 16:27:13 +0430

    adding some methods to override

--- Core/Controller.php
+++ Core/Controller.php
@@ -4,5 +4,15 @@ namespace Core;
 
 class Controller
 {
+    public function before()
+    {
+        echo 'before<br>';
+        return true;
+    }
 
+
+    public function after()
+    {
+        echo '<br>after<br>';
+    }
 }
