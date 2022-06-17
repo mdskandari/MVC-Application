fdbaa491fbe0659d430d4d079758b63615b87778
Mohammad Eskandari <mdskandari@outlook.com>
2022-06-17 16:15:31 +0430

    isolate routes

---
+++ App/Web.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+$router = new \Core\Router();
+
+$router->add('/series', ['uses' => 'SeriesController@index']);
+$router->add('/series/{slug}', 'SeriesController@serie');
+$router->add('/series/{slug}/episode/{id}', 'SeriesController@episode');//['use
+
+
+return $router;
